066a9410ffdb10bfae624b6330638b55ad6c777d
FlipFlaptheTraitor <flipflapthetraitor@protonmail.com>
2021-02-06 23:04:01 -0500

    added canvas background

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
+// context.beginPath();
+// context.rect(100, 50, 200, 150);
+// context.fillStyle = 'blue';
+// context.fill();
+// context.stroke();
 
+window.onload = function() {
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    var img = document.getElementById('source');
+
+    // set canvas to max width/height
+    context.canvas.width = window.innerWidth
+    context.canvas.height = window.innerHeight
+
+    // add image to canvas
+    context.drawImage(img, 0, 0, window.innerWidth, window.innerHeight)
+
+    // add title to canvas
+    context.font = "30px Arial";
+    context.fillText("Browser Quest", 10, 50);
+}
 let gameManager = {
     
     setGameStart: function(classType){
