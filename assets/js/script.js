28629d08788c1ab3ff4729d00f945489a8e703f0
FlipFlaptheTraitor <flipflapthetraitor@protonmail.com>
2021-02-08 19:13:54 -0500

    fixed on resize function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,36 @@
-// context.beginPath();
-// context.rect(100, 50, 200, 150);
-// context.fillStyle = 'blue';
-// context.fill();
-// context.stroke();
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+const img = document.getElementById('source');
+const img1 = document.getElementById('source1');
 
 window.onload = function() {
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
-    var img = document.getElementById('source');
-
     // set canvas to max width/height
     context.canvas.width = window.innerWidth
     context.canvas.height = window.innerHeight
-
     // add image to canvas
     context.drawImage(img, 0, 0, window.innerWidth, window.innerHeight)
+    // context.clearRect(100, 100, 50, 50)
+
+    // let flag = false
+
+    // setInterval(() => {
+    //     if (flag) {
+    //         context.drawImage(img, 0, 0, window.innerWidth, window.innerHeight)
+    //         context.fillText("Browser Quest", 10, 50);
+    //         flag = false
+    //     } else {
+    //         context.drawImage(img1, 100, 100, 50, 50)
+    //         flag = true
+    //     }
+    // }, 175);
+
 
     // add title to canvas
-    context.font = "30px Arial";
+    context.font = "60px Luminari";
+    context.fillStyle = "white";
     context.fillText("Browser Quest", 10, 50);
 }
+
 window.onresize = function() {
     // set canvas to adjust with screen size
     context.canvas.width = window.innerWidth
